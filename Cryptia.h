abb36985bacdb4b4723397d43637b00c02b27abf
okada <kokuzen@gmail.com>
2014-02-14 13:26:37 +0900

    fix CkcRandom test

---
+++ Cryptia.h
@@ -0,0 +1,11 @@
+#ifndef CRYPTIA_CRYPTIA_H
+#define CRYPTIA_CRYPTIA_H
+//Cryptia:20140214
+#include <iostream>
+#include "common_key/Aes.h"
+#include "common_key/mode/CtrMode.h"
+#include "asymmetric_key/Rsa.h"
+#include "random/CkcRandom.h"
+
+
+#endif //CRYPTIA_CRYPTIA_H
--- random/CkcRandom.cpp
+++ random/CkcRandom.cpp
@@ -10,17 +10,20 @@ int main(int argc, char* argv[])
 {
 	const auto random = CkcRandom::Create(common_key::Aes::Create());
 	random->Initialize(ByteArray(48, 1));
-	auto random_array = random->GenerateRandomByteArray(9999);
+	auto random_array = random->GenerateRandomByteArray(1000);
 	std::vector<int> histgram(256, 0);
+	std::cout << "random:{";
 	for(const auto random_byte : random_array){
 		++histgram.at(random_byte);
-		std::cout << static_cast<int>(random_byte) << std::endl;
+		std::cout << static_cast<int>(random_byte) << ", ";
 	}
-	/*
+	std::cout << "}" << std::endl;
+
+	std::cout << "hist:{";
 	for(auto h : histgram){
-		std::cout << h << std::endl;
+		std::cout << h << ", ";
 	}
-	*/
+	std::cout << "}" << std::endl;
 
     return 0;
 }
